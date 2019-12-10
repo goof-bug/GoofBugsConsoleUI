ae05df2fee6003e2ed5db9d8e9001653cd7cf6a6
goof bug <gurra2004@gmail.com>
2019-12-10 10:42:11 +0100

    Added title functionality and commented code.

--- GoofBugsConsoleUI/MultipleChoiceList.cs
+++ GoofBugsConsoleUI/MultipleChoiceList.cs
@@ -4,23 +4,45 @@ using System.Text;
 
 namespace GoofBugsConsoleUI
 {
+    /// <remarks>A class for creating and letting the user interacting with multiple choice lists.</remarks>
     public class MultipleChoiceList
     {
+        string title;
+
         string[] choices;
 
         char leftCharacter = '>';
         char rightCharacter = '<';
 
         bool escapable;
-        public MultipleChoiceList(string[] _choices, bool _escapable)
+
+        /// <summary>
+        /// The counstructor for a new multiple choice list.
+        /// </summary>
+        /// <param name="_title">A string with the title/question that the user will be shown together with the choices.</param>
+        /// <param name="_choices">A string array containing all the choices the user can choose from.</param>
+        /// <param name="_escapable">A bool that decides if the user can use esc to escape out of the selection without making a choice.</param>
+        public MultipleChoiceList(string _title, string[] _choices, bool _escapable)
         {
+            title = _title;
+
             choices = _choices;
 
             escapable = _escapable;
         }
 
-        public MultipleChoiceList(string[] _choices, bool _escapable, char _leftCharacter, char _rightCharacter)
+        /// <summary>
+        /// The counstructor for a new multiple choice list.
+        /// </summary>
+        /// <param name="_title">A string with the title/question that the user will be shown together with the choices.</param>
+        /// <param name="_choices">A string array containing all the choices the user can choose from.</param>
+        /// <param name="_escapable">A bool that decides if the user can use esc to escape out of the selection without making a choice.</param>
+        /// <param name="_leftCharacter">A char that will be printed to the left of the currently selected choice.</param>
+        /// <param name="_rightCharacter">A char that will be printed to the right of the currently selected choice.</param>
+        public MultipleChoiceList(string _title, string[] _choices, bool _escapable, char _leftCharacter, char _rightCharacter)
         {
+            title = _title;
+
             choices = _choices;
 
             escapable = _escapable;
@@ -29,15 +51,25 @@ namespace GoofBugsConsoleUI
             rightCharacter = _rightCharacter;
         }
 
+        /// <summary>
+        /// Method that starts a loop where the user can navigate and select a choice and then returns the result.
+        /// </summary>
+        /// <returns>a string with the choice the user choose, if the user escaped the selection this returns null.</returns>
         public string InitiateSelection()
         {
-            bool foobar = false;
+            bool selectionMade = false;
+
+            // int that keeps track of the current selection.
             int currentSelection = 0;
 
-            while (!foobar)
+            // Continue looping until the user has made a valid selection or escaped.
+            while (!selectionMade)
             {
                 Console.Clear();
 
+                Console.WriteLine(title);
+
+                //Write the choiches to the screen with the current selection highlighted.
                 for(int i = 0; i < choices.Length; i++)
                 {
                     if(i == currentSelection)
@@ -59,21 +91,24 @@ namespace GoofBugsConsoleUI
                     }
                 }
 
+                //Let the user navigate the selections.
+                
                 ConsoleKey keyPress = Console.ReadKey().Key;
 
                 switch (keyPress)
                 {
                     case ConsoleKey.UpArrow:
+                        //Make sure the selection is within bounds.
                         if (currentSelection != 0) currentSelection--;
-                        //currentSelection--;
                         break;
                     case ConsoleKey.DownArrow:
+                        //Make sure the selection is within bounds.
                         if (currentSelection != choices.Length - 1) currentSelection++;
-                        //currentSelection++;
                         break;
                     case ConsoleKey.Enter:
                         return choices[currentSelection];
                     case ConsoleKey.Escape:
+                        //Only let the user escape if the list is configured as escapeable.
                         if (escapable) return null;
                         break;
                     default:
